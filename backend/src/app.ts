e3a25c9a84c1109df38c695719c43eb33bb51335
Skolli <tanusuch@gmail.com>
2026-03-09 19:12:15 -0400

    Create foundation for backend, Node/TS setup, Express server, testing server health route

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import healthRoutes from "./routes/health.routes";
+import authRoutes from "./routes/auth.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/health", healthRoutes);
+app.use("/api/auth", authRoutes);
+
+export default app;
