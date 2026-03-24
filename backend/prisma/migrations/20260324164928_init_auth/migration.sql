5c9829ac296033ccc92ddcc0417a621bdec4bdc6
Skolli <tanusuch@gmail.com>
2026-03-24 14:08:58 -0400

    add initial databse migration for user model

---
+++ backend/prisma/migrations/20260324164928_init_auth/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('TEACHER', 'STUDENT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
